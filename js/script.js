52ab417f11c8cc7c73f9fa7b49175541d8192738
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2023-07-24 00:31:16 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -55,3 +55,55 @@ $tabBtn.forEach((item) => {
     lastActiveTabBtn = this;
   });
 });
+
+//////////////////////////////////////////
+// Отримуємо всі елементи з класом "card"
+const cards = document.querySelectorAll(".card");
+
+// Додаємо обробник події "click" до кожного елемента "card"
+cards.forEach(function (card) {
+  const imgHolder = card.querySelector(".img-holder");
+  const img = imgHolder.querySelector("img");
+
+  card.addEventListener("click", function () {
+    if (!card.classList.contains("active")) {
+      // Створюємо модальне вікно
+      const modal = document.createElement("div");
+      modal.classList.add("modal");
+
+      // Створюємо зображення для модального вікна
+      const modalImg = document.createElement("img");
+      modalImg.src = img.src;
+      modalImg.alt = img.alt;
+      modalImg.classList.add("modal-img");
+
+      // Додаємо зображення до модального вікна
+      modal.appendChild(modalImg);
+
+      // Додаємо модальне вікно до body
+      document.body.appendChild(modal);
+
+      // Використовуємо setTimeout, щоб додати клас "enlarged" після додавання модального вікна до DOM, щоб спрацювали CSS-переходи
+      setTimeout(function () {
+        modalImg.classList.add("enlarged");
+      }, 0);
+
+      // Змінюємо клас "active" у карточці, щоб зберегти стан відкритого модального вікна
+      card.classList.add("active");
+
+      // Додаємо обробник події "click" до зображення у модальному вікні для зменшення зображення при повторному кліку
+      modalImg.addEventListener("click", function () {
+        modal.remove();
+        card.classList.remove("active");
+      });
+    } else {
+      // Закриття модального вікна при повторному кліку на карточку
+      const modal = document.querySelector(".modal");
+      if (modal) {
+        modal.remove();
+      }
+      card.classList.remove("active");
+    }
+  });
+});
+////////////////////////////////////////////////
