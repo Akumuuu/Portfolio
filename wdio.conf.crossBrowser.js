be77f84a7abb924cfb7817f996a9f1a94ce103b6
Yura <yurii_k@oss-usa.com>
2024-10-21 18:57:35 +0300

    HI

--- wdio.conf.crossBrowser.js
+++ wdio.conf.crossBrowser.js
@@ -11,7 +11,7 @@ config: {
     {
       browserName: 'chrome',
       'goog:chromeOptions': {
-        args: [ '--window-size=1920,1080'],
+        args: ['--headless', '--disable-dev-shm-usage', '--no-sandbox', '--disable-gpu', '--window-size=1920,1080', '--allow-insecure-localhost'],
       },
     },
     // {
