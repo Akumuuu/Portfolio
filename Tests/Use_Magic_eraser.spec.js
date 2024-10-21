d89e9461927cf93ab06d19bc404fce77daeff5f2
Yura <yurii_k@oss-usa.com>
2024-10-21 18:44:35 +0300

    dds

--- Tests/Use_Magic_eraser.spec.js
+++ Tests/Use_Magic_eraser.spec.js
@@ -5,6 +5,7 @@ describe('Pixel 8a Use Magic eraser Tests', () => {
 
     it('Open Pixel 8a Use Magic eraser', async () => {
         await browser.url('https://akumuuu.github.io/Portfolio/')
+        await expect(browser).toContain('https://akumuuu.github.io/Portfolio/')
     })
 
 
