07365db66a350caf894f08f86e068f85424674f0
Yura <yurii_k@oss-usa.com>
2024-10-24 16:12:23 +0300

    alure uodate

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,21 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
-    "chromeWebSecurity": false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      
+      // Підключення Allure плагіну
+      allureWriter(on, config);
+
+      // Інші Node події можна додавати тут
+      return config;
     },
-    specPattern:'cypress/e2e/**/*.{js,jsx,ts,tsx}'
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
   },
   env: {
-    allureResultsPath: 'allure-results'  // Path for saving Allure results
+    allureResultsPath: 'allure-results'  // Шлях для збереження результатів Allure
   },
 });
